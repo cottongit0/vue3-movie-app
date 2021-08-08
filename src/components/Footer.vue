26c77394d0e5a752afd4d17e7f0753e4312a2396
cottongit0 <cottongit0@gmail.com>
2021-08-08 23:19:13 +0900

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href=""
+      target="_blanck">
+      (c){{ new Date().getFullYear() }}</a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo"
+
+export default {
+  components:{
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer{
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo{
+    display: block;
+    margin-bottom: 4px;
+  };
+  a{
+    text-decoration: none;
+  }
+}
+</style>
